d054aee6f101732dcede7123254b40441994c34c
ahmed <ahmed.akakzia@gmail.com>
2022-05-12 17:59:32 +0200

    update job runner

--- run_jobs.py
+++ run_jobs.py
@@ -14,39 +14,37 @@ scratch = os.environ['SCRATCH']
 mkdir_p(job_directory)
 
 nb_seeds = 1
-models = ['full_gn']
 
 for i in range(nb_seeds):
-    for model in models:
-        job_file = os.path.join(job_directory, "test_{}%.slurm".format(model))
+    job_file = os.path.join(job_directory, "first_stage%.slurm")
 
-        with open(job_file, 'w') as fh:
-            fh.writelines("#!/bin/bash\n")
-            fh.writelines("#SBATCH --account=kcr@v100\n")
-            fh.writelines("#SBATCH --job-name=test_{}\n".format(model))
-            fh.writelines("#SBATCH --qos=qos_gpu-t3\n")
-            fh.writelines("#SBATCH --output=test_{}%_%j.out\n".format(model))
-            fh.writelines("#SBATCH --error=test_{}%_%j.out\n".format(model))
-            fh.writelines("#SBATCH --time=19:59:59\n")
-            fh.writelines("#SBATCH --ntasks=24\n")
-            fh.writelines("#SBATCH --ntasks-per-node=1\n")
-            fh.writelines("#SBATCH --gres=gpu:1\n")
-            fh.writelines("#SBATCH --hint=nomultithread\n")
-            fh.writelines("#SBATCH --array=0-0\n")
+    with open(job_file, 'w') as fh:
+        fh.writelines("#!/bin/bash\n")
+        fh.writelines("#SBATCH --account=kcr@v100\n")
+        fh.writelines("#SBATCH --job-name=first_stage\n")
+        fh.writelines("#SBATCH --qos=qos_gpu-t3\n")
+        fh.writelines("#SBATCH --output=first_stage%_%j.out\n")
+        fh.writelines("#SBATCH --error=first_stage%_%j.out\n")
+        fh.writelines("#SBATCH --time=19:59:59\n")
+        fh.writelines("#SBATCH --ntasks=24\n")
+        fh.writelines("#SBATCH --ntasks-per-node=1\n")
+        fh.writelines("#SBATCH --gres=gpu:1\n")
+        fh.writelines("#SBATCH --hint=nomultithread\n")
+        fh.writelines("#SBATCH --array=0-0\n")
 
-            fh.writelines("module load pytorch-gpu/py3/1.4.0\n")
+        fh.writelines("module load pytorch-gpu/py3/1.4.0\n")
 
-            fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
-            fh.writelines("export LIBRARY_PATH=$LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
-            fh.writelines("export CPATH=$CPATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/include\n")
-            fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/genisi01/uqy56ga/.mujoco/mujoco200/bin\n")
-            fh.writelines("export OMPI_MCA_opal_warn_on_missing_libcuda=0\n")
-            fh.writelines("export OMPI_MCA_btl_openib_allow_ib=1\n")
-            fh.writelines("export OMPI_MCA_btl_openib_warn_default_gid_prefix=0\n")
-            fh.writelines("export OMPI_MCA_mpi_warn_on_fork=0\n")
+        fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
+        fh.writelines("export LIBRARY_PATH=$LIBRARY_PATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/lib\n")
+        fh.writelines("export CPATH=$CPATH:/gpfslocalsup/spack_soft/mesa/18.3.6/gcc-9.1.0-bikg6w3g2be2otzrmyy43zddre4jahme/include\n")
+        fh.writelines("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/linkhome/rech/genisi01/uqy56ga/.mujoco/mujoco200/bin\n")
+        fh.writelines("export OMPI_MCA_opal_warn_on_missing_libcuda=0\n")
+        fh.writelines("export OMPI_MCA_btl_openib_allow_ib=1\n")
+        fh.writelines("export OMPI_MCA_btl_openib_warn_default_gid_prefix=0\n")
+        fh.writelines("export OMPI_MCA_mpi_warn_on_fork=0\n")
 
-            fh.writelines("srun python -u -B train.py --algo 'semantic' --n-blocks 2 --n-epochs 1000 --n-cycles 50 --n-batches 30 --architecture {} --save-dir 'test_{}/' 2>&1 ".format(model, model))
+        fh.writelines("srun python -u -B train.py --algo 'semantic' --n-blocks 2 --n-epochs 100 --n-cycles 50 --n-batches 30 --architecture 'full_gn' --save-dir 'first_stage/' 2>&1 ")
 
-        os.system("sbatch %s" % job_file)
-        sleep(1)
+    os.system("sbatch %s" % job_file)
+    sleep(1)
         
