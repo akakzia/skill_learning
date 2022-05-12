ef818d43667aab67f9ada6b80206c28a581b556f
ahmed <ahmed.akakzia@gmail.com>
2022-05-12 11:13:35 +0200

    remove outdated arguments

--- utils.py
+++ utils.py
@@ -13,7 +13,6 @@ def init_storage(args):
         os.mkdir(args.save_dir)
     # path to save the model
     logdir = os.path.join(args.save_dir, '{}_{}_{}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), args.env_name, args.architecture))
-    logdir += '_{}'.format(args.reward_type)
     # path to save evaluations
     model_path = os.path.join(logdir, 'models')
     if not os.path.exists(logdir):
